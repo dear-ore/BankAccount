a2d0aa1e47fa42a8a87cb2df1c412295c5caf4ac
Adore-dot <akinolaoreoluwa@gmail.com>
2026-01-16 14:34:48 +0100

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿namespace BankAccount
+{ 
+    public class BankAccount
+    {
+        public string AccountNumber { get; }
+        public string AccountHolder { get; set; }
+        public int Balance { get; }
+        public BankAccount(string name, string accnumber, int initialBal)
+        {
+            AccountHolder = name;
+            AccountNumber = accnumber;
+            Balance = initialBal;
+        }
+
+        public int Deposit(int amount)
+        {
+            int newBalance = Balance + amount;
+            return newBalance;
+        }
+
+        public int Withdraw(int amount)
+        {
+            if(amount > Balance)
+            {
+                throw new ArgumentOutOfRangeException($"The amount ({amount}) you're trying to withdraw is larger than your current balance. Try again with a smaller amount");
+            }
+
+            return Balance - amount;
+        }
+    }
+}
