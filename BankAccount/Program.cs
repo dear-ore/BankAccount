55e7be915fb8d5c3e1139ba87e16ca19ec7009ac
Adore-dot <akinolaoreoluwa@gmail.com>
2026-01-20 21:11:34 +0100

    Completed the tests for all methods in Savings and BankAccount

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,21 +1,48 @@
 ﻿using BankAccounts;
 
-Console.WriteLine("=======Testing Bank Account OOP=============");
+//Console.WriteLine("=======Testing Bank Account OOP=============");
 
-BankAccount newCustomer = new BankAccount("Tina Turner", "1234567890", 30000m);
+//BankAccount newCustomer = new BankAccount("Tina Turner", "1234567890", 30000m);
+//Console.WriteLine($"Account Holder - {newCustomer.AccountHolder}");
+//Console.WriteLine($"Account Number - {newCustomer.AccountNumber}");
+//Console.WriteLine($"Account Balance - {newCustomer.Balance}");
+
+
+//Console.WriteLine("=================TESTING BANK ACCOUNT METHODS===================");
+//newCustomer.Deposit(20000m);
+//Console.WriteLine($"New balance after 20,000 deposit {newCustomer.Balance}"); //should be 50,000
+
+//newCustomer.Withdraw(10000m);
+//Console.WriteLine($"New balance after 10,000 withdrawal {newCustomer.Balance}"); //should be 40,000
+
+////test CheckBalance method
+
+//Console.WriteLine($"Latest balance is {newCustomer.CheckBalance()}");
+
+Console.WriteLine("==================Testing Savings Account Methods================");
+SavingsAccount newCustomer = new SavingsAccount("Tina Turner", "1234567890", 0, 0.05m);
 Console.WriteLine($"Account Holder - {newCustomer.AccountHolder}");
 Console.WriteLine($"Account Number - {newCustomer.AccountNumber}");
 Console.WriteLine($"Account Balance - {newCustomer.Balance}");
 
 
 Console.WriteLine("=================TESTING BANK ACCOUNT METHODS===================");
-newCustomer.Deposit(20000m);
-Console.WriteLine($"New balance after 20,000 deposit {newCustomer.Balance}"); //should be 50,000
+newCustomer.Deposit(40000m);
+Console.WriteLine($"New balance after 20,000 deposit {newCustomer.Balance}"); 
 
-newCustomer.Withdraw(10000m);
-Console.WriteLine($"New balance after 10,000 withdrawal {newCustomer.Balance}"); //should be 40,000
+newCustomer.Withdraw(50000m);
+Console.WriteLine($"New balance after 10,000 withdrawal {newCustomer.Balance}"); 
 
-//test CheckBalance method
 
 Console.WriteLine($"Latest balance is {newCustomer.CheckBalance()}");
+
+Console.WriteLine($"Interest on latest balance is {newCustomer.CalculateInterest()}");
+
+newCustomer.ApplyInterest();
+
+Console.WriteLine($"Balance after applying interest is {newCustomer.CheckBalance()}");
+
+Console.WriteLine("=====================TESTING EXCEPTION HANDLING============================================");
+
+
 
